9a433c4264b7d09b5500dcfee4ef5083ea968fb9
jo2544 <gaangae@gmail.com>
2021-12-04 08:40:51 +0900

    자바 스크립트

--- js/tetris.js
+++ js/tetris.js
@@ -1,8 +1,45 @@
+//DOM
+
 const playground = document.querySelector(".playground > ul");
 
-console.log(playground)
+//Setting
+const GAME_ROMS = 20;
+const GAME_COLS = 10;
 
+// variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+    tree: [
+        [[0,0],[0,1],[1,0],[1,1]],
+        [],
+        [],
+        [],
+    ]
+}
+
+const MovingItem = {
+    type: "tree",
+    direction: 0,
+    top: 0,
+    left: 0,
+};
+
+init()
+
+//function
+function init(){
+    tempMovingItem = {...MovingItem };
 for (let i = 0; i < 20; i++) {
+        prependNewLine()
+    }   
+    renderBlocks()
+}
+
+function prependNewLine(){
     const li = document.createElement("li");
     const ul = document.createElement("ul");
     for(let j=0; j<10; j++){
@@ -11,4 +48,13 @@ for (let i = 0; i < 20; i++) {
     }
     li.prepend(ul)
     playground.prepend(li)
+}
+function renderBlocks(){
+    const { type, direction, top, left } = tempMovingItem;
+    
+    BLOCKS[type][direction].forEach(block=>{
+        const x = block[0];
+        const y = block[1];
+        const target = playground.childNodes;
+    })
 }
